2309c867fd558583b253c6f2c9af6d55a036ea38
maiANKIT <ankitsharmaindiaa@gmail.com>
2026-04-05 20:05:00 +0530

    done

---
+++ kthElementoftwoarray.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cout<<"enter size of a: ";
+    cin>>n;
+
+    int m;
+    cout<<"enter size of b: ";
+    cin>>m;
+
+    vector<int> a(n);
+    cout<<"enter values of a: ";
+    for(int i = 0; i<a.size(); i++){
+        cin>>a[i];
+    }
+
+    vector<int> b(m);
+    cout<<"enter values of b: ";
+    for(int i = 0; i<b.size(); i++){
+        cin>>b[i];
+    }
+
+    int k;
+    cout<<"enter k: ";
+    cin>>k;
+
+    int i = 0;
+    int j = 0;
+    int x = 0;
+    int y = -1;
+    while(k>x){
+
+        if(a[i] > b[j]){
+            
+        }
+
+    }
+
+   return 0;
+}
